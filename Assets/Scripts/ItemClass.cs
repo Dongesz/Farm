32ddd02fb2959bbb362d53a99d05d3e4f6ac027a
Dongesz <majormate12.com>
2024-12-07 00:33:07 +0100

    Started working on a inventory system using classes

---
+++ Assets/Scripts/ItemClass.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public abstract class ItemClass : ScriptableObject
+{
+    [Header("Item")]
+    public string itemName;
+    public Sprite itemIcon;
+
+    public abstract ItemClass GetItem();
+    public abstract ToolClass GetTool();
+    public abstract MiscClass GetMisc();
+    public abstract ConsumableClass GetConsumable();
+}
